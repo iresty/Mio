9458abcc830046408a17a0e87796466935fba78b
sumory <sumory.wu@gmail.com>
2016-08-09 14:29:35 +0800

    add `error message` for error stack

--- resty/thirdparty/lor/lor/lib/router/layer.lua
+++ resty/thirdparty/lor/lor/lib/router/layer.lua
@@ -78,22 +78,22 @@ function Layer:new(path, options, fn, fn_args_length)
     return instance
 end
 
-function Layer:handle_error(error, req, res, next)
-    debug("layer.lua#handel_error:", self, error)
+function Layer:handle_error(err, req, res, next)
+    debug("layer.lua#handel_error:", self, err)
     local fn = self.handle
 
     -- a property named 'length' to indicate its args length
     if self.length ~= 4 then
         debug("not match handle_error")
-        next(error)
+        next(err)
         return
     end
 
     local e
-    local ok= xpcall(function() 
-        fn(error, req, res, next) 
+    local ok = xpcall(function() 
+        fn(err, req, res, next) 
     end,  function()
-        e = traceback()
+        e = (err or "") .. "\n" .. traceback()
     end)
     --print(random() .. "  layer.lua - Layer:handle_error", "ok?", ok, "error:", e, "pcall_error:", e, "layer.name:", self.name)
 
@@ -114,16 +114,16 @@ function Layer:handle_request(req, res, next)
 
     --local trackId = random()
     local e
-    local ok= xpcall(function() 
+    local ok, ee = xpcall(function() -- add `ee` for final handler logic
         fn(req, res, next) 
-    end, function()
-        e = traceback()
+    end, function(msg)
+        e = (msg or "") .. "\n" .. traceback()
     end)
     --debug(trackId .. "  layer.lua - Layer:handle_request-", "ok?", ok, "error:", e, "layer.name:", self.name, "middle_type:", self.length)
 
     if not ok then
-        debug("handle_request:pcall error", ok, e)
-        next(e)
+        debug("handle_request:call error", ok, e, ee)
+        next(e or ee)
     end
 end
 
